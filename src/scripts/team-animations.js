85f7e0a07bad161031df13d01ba94d36f49c386c
RettPrattr <desaswork@gmail.com>
2022-12-28 19:48:40 +0300

    cards scroll animation is completed

--- src/scripts/team-animations.js
+++ src/scripts/team-animations.js
@@ -3,22 +3,44 @@ $(document).ready(function () {
 
     if(location.href.indexOf('team-page')!==-1) {
 
-        var cards = $(".card-item")
+        var cards = $(".card-item");
         var cards_count = cards.length;
+
+        /////// FOR CARDS OF SECOND LEVEL
+
+        var cards_2 = $(".card-item");
+        var cards_2_count = cards.length;
+
+
+        /////// TRIGGERS FOR EVERY CARD
+
+        $(".card-item").each(function (card_item_index) {
+            $(this).addClass("card-item-index-" + card_item_index)
+    
+            $(this).prepend('<div class="trigger-for-card-item-' + card_item_index + ' trigger"></div>');
+    
+        })
+
+        $(".card-item-2").each(function (card_item_2_index) {
+            $(this).addClass("card-item-2-index-" + card_item_2_index)
+    
+            $(this).prepend('<div class="trigger-for-card-item-2-' + card_item_2_index + ' trigger"></div>');
+    
+        })
         
         /////// LOGO APPEARANCE
 
         var controller_team_anim = new ScrollMagic.Controller();
 
         var tl_logo_appearance = new TimelineMax();
-        tl_logo_appearance.fromTo(".logo-and-cards", { opacity: 0 }, {opacity: 1, duration: 0.8, ease: "power2.out" });
+        tl_logo_appearance.fromTo(".logo-and-cards", { opacity: 0 }, {opacity: 1, duration: 0.8, ease: "power2.out"});
     
     
         var scene = new ScrollMagic.Scene({
             offset: 0,
             triggerElement: ".logo-and-cards",
             triggerHook: 0.7,
-            duration: 0,
+            duration: 20,
             reverse: false
         })
             .setTween(tl_logo_appearance)
@@ -47,33 +69,64 @@ $(document).ready(function () {
 
         /////// CARDS APPEARANCE   
 
-
         var controller_cards_anim = new ScrollMagic.Controller();
 
-        $(".cards").each(function(i) {
-          
-          var animateIn = new TimelineMax();
-          var imgOvarly = $(this).find(".card-item");
-          console.log(imgOvarly);
-          animateIn
-          .staggerFromTo(imgOvarly, 0.2, 
-                         {opacity: 0, scale: 0.6},
-                         {opacity: 1, scale: 1}, 0.3);
-          
-          var scene = new ScrollMagic.Scene({ 
-            triggerElement: this,
-            triggerHook: 0.7,
-            offset: "0",
-            // reverse: false
-            })
-          
-          .setTween(animateIn).addTo(controller_cards_anim)
-        //   .addIndicators()
-            // var reverse = scene.reverse();
-            // scene.reverse(true)
-            }); 
+        for (card_item_index = 0; card_item_index < cards_count; card_item_index++) { 
+            $(".card-item-index-" + card_item_index).each(function () { 
+                var tl_card_appearance = new TimelineMax();
+                tl_card_appearance.fromTo(this, { opacity: 0, scale: 0.6 }, {opacity: 1, scale: 1}, 0.15 * card_item_index);
 
-    }
+                var scene = new ScrollMagic.Scene({ 
+                triggerElement: ".trigger-for-card-item-" + card_item_index,
+                triggerHook: 0.7,
+                offset: "0",
+                reverse: true
+                })
+              
+              .setTween(tl_card_appearance)
+              .addTo(controller_cards_anim)
+            })
+
+        }
+
+
+        var controller_cards_2_anim = new ScrollMagic.Controller();
+
+        for (card_item_2_index = 0; card_item_2_index < cards_2_count; card_item_2_index++) { 
+            $(".card-item-2-index-" + card_item_2_index).each(function () { 
+                var tl_card_2_appearance = new TimelineMax();
+                tl_card_2_appearance.fromTo(this, { opacity: 0, scale: 0.6 }, {opacity: 1, scale: 1}, 0.15 * card_item_2_index);
+
+                var scene = new ScrollMagic.Scene({ 
+                triggerElement: ".trigger-for-card-item-2-" + card_item_2_index,
+                triggerHook: 0.7,
+                offset: "0",
+                reverse: true
+                })
+              
+              .setTween(tl_card_2_appearance)
+              .addTo(controller_cards_2_anim)
+            })
+
+        }
+
+
+
+        // $(".cards").each(function(i) {
+          
+        //   var animateIn = new TimelineMax();
+        //   var imgOvarly = $(this).find(".card-item");
+        //   console.log(imgOvarly);
+        //   animateIn
+        //   .staggerFromTo(imgOvarly, 0.2, 
+        //                  {opacity: 0, scale: 0.6},
+        //                  {opacity: 1, scale: 1}, 0.3);
+          
+
+        //   .addIndicators()
+        //     var reverse = scene.reverse();
+        //     scene.reverse(true)
+        //     }); 
 
 
         /////// PACKSHOT GO AWAY  
@@ -86,7 +139,7 @@ $(document).ready(function () {
             triggerElement: ".team-cover-block ",
             triggerHook: 0.5,
             duration: 0,
-            reverse: false
+            // reverse: false
         })
         // .addIndicators()
         .setClassToggle(".packshot", "away")
@@ -106,11 +159,15 @@ $(document).ready(function () {
             triggerElement: ".team-cover-block",
             triggerHook: 0.7,
             duration: 0,
-            reverse: false
+            // reverse: false
         })
             .setTween(tl_right_text_appearance)
             .addTo(controller_right_text_anim)
             .addIndicators()
+    }
+
+
+
 })
 
 
